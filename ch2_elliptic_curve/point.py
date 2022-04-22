a8f5d2486e0aa5348c9a141c7987ac1625a79e3c
parkchoongho <pchyo92@gmail.com>
2022-04-23 08:45:10 +0900

    ch2.3까지

---
+++ ch2_elliptic_curve/point.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        if self.y ** 2 != self.x ** 3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y or self.a != other.a or self.b != other.b
+
+
+print(Point(-1, -1, 5, 7) == Point(-1, -1, 5, 7))
