e0f6d913affd7c0def058374d690c4fbd4bd148f
parkchoongho <pchyo92@gmail.com>
2022-04-25 22:40:07 +0900

    2장 완료

--- ch2_elliptic_curve/point.py
+++ ch2_elliptic_curve/point.py
@@ -4,6 +4,8 @@ class Point:
         self.y = y
         self.a = a
         self.b = b
+        if self.x is None and self.y is None:
+            return
         if self.y ** 2 != self.x ** 3 + a * x + b:
             raise ValueError('({}, {}) is not on the curve'.format(x, y))
 
@@ -12,6 +14,29 @@ class Point:
 
     def __ne__(self, other):
         return self.x != other.x or self.y != other.y or self.a != other.a or self.b != other.b
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s ** 2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+        if self == other:
+            s = (3 * (self.x ** 2) + self.a) / (2 * self.y)
+            x = s ** 2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
 
 
 print(Point(-1, -1, 5, 7) == Point(-1, -1, 5, 7))
