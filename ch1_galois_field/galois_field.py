55f87db9814c57a9ed4c5ba474b517ca545f0a11
parkchoongho <pchyo92@gmail.com>
2022-07-31 19:25:54 +0900

    🧑‍💻 study ch1 유한체

--- ch1_galois_field/galois_field.py
+++ ch1_galois_field/galois_field.py
@@ -50,9 +50,7 @@ class FieldElement:
         return self.__class__(num, self.prime)
 
 
-a = FieldElement(7, 13)
-b = FieldElement(6, 13)
+a = FieldElement(3, 5)
+b = FieldElement(2, 5)
 
-print(a == b)
-print(a == a)
-print(a != b)
+print(a ** -1)
